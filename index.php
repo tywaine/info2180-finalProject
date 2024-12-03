72a431089f4b21fceeb9ac393680d0e2aa74f4df
tywaine <peterstywaine@gmail.com>
2024-12-03 01:51:45 -0500

    Add User css applied.. looking good.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ User::loadUsers();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>🐬 Dolphin CRM</title>
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Symbols+Outlined" />
 </head>
